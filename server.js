f2fb3198aa1772aab393b8895f81e167af511be6
Ahmed Abdo <devahmedabdo@gmail.com>
2025-06-24 04:06:38 +0300

    remove auth

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
-const sanitizeInput = require('./middlewares/sanitizeInputMiddleware');
+// const sanitizeInput = require('./middlewares/sanitizeInputMiddleware');
 // Local
 const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/errorHandler');
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10mb' }));
- app.use(sanitizeInput);
+//  app.use(sanitizeInput);
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
 // Routes
